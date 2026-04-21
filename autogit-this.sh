b2411f258764e3d8b7621e0aba24e8d49f77aa29
mortyhawky <morty.hawky@outlook.com>
2026-04-21 12:20:17 +0200

    2026-04-21T12:20:17+02:00

--- autogit-this.sh
+++ autogit-this.sh
@@ -6,10 +6,12 @@ echo "Start: $SCRIPT_PATH"
 source $ZDOTDIR/functions.zsh
 
 
+echo "command:  gh auth status"
 gh auth status |bat
 f_pause
 
-git status -v |grep -i --color=always modified |bat
+echo "command:  git status"
+git status  |grep -i --color=always modified |bat
 f_pause
 
 git add . -v |bat
