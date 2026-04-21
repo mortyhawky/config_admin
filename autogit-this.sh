dbeca6c64e6367ac03d1d9313853f1757588711e
mortyhawky <morty.hawky@outlook.com>
2026-04-21 12:13:34 +0200

    2026-04-21T12:13:34+02:00

--- autogit-this.sh
+++ autogit-this.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env zsh
-## Updated ArchReal 2026-04-21 11:47 Tue
+## Updated ArchReal 2026-04-21 12:12 Tue
 local SCRIPT_PATH="${(%)-%N}"
 echo "Start: $SCRIPT_PATH"
 
 source $ZDOTDIR/functions.zsh
 
+
 gh auth status |bat
 f_pause
+
 git status -v |grep -i --color=always modified |bat
 f_pause
+
 git add . -v |bat
 f_pause
+
 git commit -m "$(date -Iseconds)" -v |bat
 f_pause
+
 git push -v |bat
