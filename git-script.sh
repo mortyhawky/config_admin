8a5d87566391e3d55ec4a87682f0411bb7e6cf38
mortyhawky <morty.hawky@outlook.com>
2026-04-21 11:48:55 +0200

    2026-04-21T11:48:55+02:00

--- git-script.sh
+++ git-script.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env zsh
+## Updated ArchReal 2026-04-21 11:47 Tue
 
-#gh auth status
-#git status
+local SCRIPT_PATH="${(%)-%N}"
+echo "Start: $SCRIPT_PATH"
+
+gh auth status
+f_pause
+git status
 git add .
 git commit -m "$(date -Iseconds)"
 git push
-return
+#return
