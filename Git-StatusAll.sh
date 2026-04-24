adff8e73b676cf14e44216ec92715da64099febb
mortyhawky <morty.hawky@outlook.com>
2026-04-24 09:10:02 +0200

    2026-04-24T09:10:02+02:00

--- Git-StatusAll.sh
+++ Git-StatusAll.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env zsh
-
-clear 
+## Updated ArchReal 2026-04-24 09:08 Fri
+local SCRIPT_PATH="${(%)-%N}"
+echo "Start: $SCRIPT_PATH"
 source $ZDOTDIR/functions.zsh
+#all functions in $ZDOTDIR/funcitons.zsh is prefixed with f_
+command -v bat >/dev/null && BAT=bat || BAT=cat
+#return
+#f_pause
 
-programs=(
-  "admin"
-  "alacritty"
-  "dmenu"
-  "dwm"
-)
+# entry point
+#clear
+SCRIPT_DIR=${0:A:h}   # just the directory
+SCRIPT_NAME=${0:t}    # :t = tail = just the filename
+echo "SCRIPT_DIR="$SCRIPT_DIR
+echo "SCRIPT_NAME="$SCRIPT_NAME
+echo
+#pause
+#return
 
-cd $XDG_CONFIG_HOME
-#ls -la
-for prg in $programs; do
-  path=$XDG_CONFIG_HOME/$prg
-  echo $path
-  #/usr/bin/rm -ri $path
-  cd $path
-  /usr/bin/git status
+# Top-level directories
+all_folders=("$XDG_CONFIG_HOME"/*(/N))
+# Folders that contain a .git directory
+git_folders=()
+
+for dir in "${all_folders[@]}"; do
+  if [[ -d "$dir/.git" ]]; then
+    git_folders+=("$dir")
+  fi
 done
 
+# Print result
+echo "\n----- GIT FOLDERS -----"
+for dir in "${git_folders[@]}"; do
+  echo "$dir"
+done
+#f_pause
+#return
+
+for dir in "${git_folders[@]}"; do
+    clear
+    echo "\n----------------------------------\n"
+    cd "$dir"
+    pwd
+    git status 
+    echo "\n----------------------------------\n"
+    f_pause
+done
