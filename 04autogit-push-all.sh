efe773cc04c9b828c76e4955dc7caaaa06651c72
mortyhawky <morty.hawky@outlook.com>
2026-04-24 08:52:06 +0200

    2026-04-24T08:52:06+02:00

--- 04autogit-push-all.sh
+++ 04autogit-push-all.sh
@@ -21,12 +21,12 @@ function git_push() {
 
 # entry point
 clear
-abs_path_r=${0:A}     # absolute resolved symlinks
-abs_path_u=${0:P}     # absolute unreolved symlinks
+#abs_path_r=${0:A}     # absolute resolved symlinks
+#abs_path_u=${0:P}     # absolute unreolved symlinks
 SCRIPT_DIR=${0:A:h}   # just the directory
 SCRIPT_NAME=${0:t}    # :t = tail = just the filename
-echo "abs_path_r="$abs_path_r
-echo "abs_path_u="$abs_path_u
+#echo "abs_path_r="$abs_path_r
+#echo "abs_path_u="$abs_path_u
 echo "SCRIPT_DIR="$SCRIPT_DIR
 echo "SCRIPT_NAME="$SCRIPT_NAME
 echo
@@ -38,16 +38,15 @@ echo
 folders=("$XDG_CONFIG_HOME"/*(/N))
 
 # Print the results
-print "Top-level folders in $XDG_CONFIG_HOME:"
-for dir in "${folders[@]}"; do
-  echo "$dir"
-done
+#print "Top-level folders in $XDG_CONFIG_HOME:"
+#for dir in "${folders[@]}"; do
+#  echo "$dir"
+#done
 #pause
 #return
 
-
-
 for dir in "${folders[@]}"; do
+    clear
     echo "----------------------------------"
     echo
     print "${dir}"          # :t gives only the folder name (basename)
