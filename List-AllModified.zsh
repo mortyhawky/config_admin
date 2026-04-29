ff4108ea1b17f00a5cecfa2178016759e2c8104b
mortyhawky <morty.hawky@outlook.com>
2026-04-29 05:31:04 +0200

    acerArch Wed Apr 29 05:31:04 CEST 2026

---
+++ List-AllModified.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+## Updated acerArch 2026-04-29 05:24 Wed
+
+source $ZDOTDIR/functions.zsh
+
+
+
+  # Iterate all folders in $XDG_CONFIG_HOME
+
+
+#l |g "\-m "
+ls -la |grep --color=auto "\-M "
+
